ac11bfa1012b49cc2bdba5d4118c65e85be3ce9a
Sebastian Messmer <mail@smessmer.de>
2015-02-18 13:30:08 +0100

    setup_biicode_project.py also works when git projectname is different to bii project name

--- setup_biicode_project.py
+++ setup_biicode_project.py
@@ -18,10 +18,11 @@ def setup_biicode():
 
 def setup_dir_structure():
   (bii_username, bii_projectname) =  bii_user_project()
+  git_projectname = os.path.basename(os.getcwd())
   os.chdir('..')
   subprocess.check_call(['bii', 'init', BUILDFOLDER])
   os.mkdir(os.path.join(BUILDFOLDER, 'blocks', bii_username))
-  shutil.move(bii_projectname, os.path.join(BUILDFOLDER, 'blocks', bii_username))
+  shutil.move(git_projectname, os.path.join(BUILDFOLDER, 'blocks', bii_username, bii_projectname))
 
 
 setup_biicode()
