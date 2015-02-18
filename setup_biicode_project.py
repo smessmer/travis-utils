a91397b1c352b1507c5d5075950f39bf8e908fc8
Sebastian Messmer <mail@smessmer.de>
2015-02-18 12:43:04 +0100

    Initial version

---
+++ setup_biicode_project.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import re, os, shutil, sys, subprocess
+
+SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+
+def bii_user_project():
+  biiconfig = open(os.path.join(SCRIPTDIR, '..', 'biicode.conf'), 'r').read()
+  username = re.search('\[parent\]\n\s+([^/]+)/', biiconfig).group(1)
+  projectname = re.search('\[parent\]\n\s+[^/]+/([^:]+)', biiconfig).group(1)
+  return (username, projectname)
+
+BUILDFOLDER = sys.argv[1]
+
+print "Buildfolder: "+BUILDFOLDER
+
+(bii_username, bii_projectname) =  bii_user_project()
+subprocess.check_call(['bii', 'init', BUILDFOLDER])
+os.mkdir(os.path.join(BUILDFOLDER, 'blocks', bii_username))
+shutil.move(bii_projectname, os.path.join(BUILDFOLDER, 'blocks', bii_username))
---
+++ setup_biicode_project.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+BUILDFOLDER="travisbuild"
+SCRIPT_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+# Download python script
+wget -P "$SCRIPT_DIR" https://raw.githubusercontent.com/smessmer/travis-utils/master/setup_biicode_project.py
+chmod +x $SCRIPT_DIR/setup_biicode_project.py
+
+cd ..
+$SCRIPT_DIR/setup_biicode_project.py $BUILDFOLDER
+cd $BUILDFOLDER
