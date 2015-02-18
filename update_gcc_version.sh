5dc9f653a93488c04a3f8a80f30818a0cc98c0ea
Sebastian Messmer <mail@smessmer.de>
2015-02-18 12:51:14 +0100

    update_gcc_version.sh takes gcc version as argument

--- update_gcc_version.sh
+++ update_gcc_version.sh
@@ -3,7 +3,7 @@ set -ev
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -qq
-if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.9; fi
-if [ "$CXX" = "g++" ]; then export CXX="g++-4.9" CC="gcc-4.9"; fi   
-if [ "$CXX" = "g++-4.9" ]; then sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.9 90; fi
+if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-$1; fi
+if [ "$CXX" = "g++" ]; then export CXX="g++-$1" CC="gcc-$1"; fi   
+if [ "$CXX" = "g++-$1" ]; then sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-$1 90; fi
 
