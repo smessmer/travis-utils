69071843a17ddc5067498e0f958f1e0ae972b7ef
Sebastian Messmer <mail@smessmer.de>
2015-02-18 12:53:51 +0100

    setup_biicode_project.py is meant to be downloaded by travis into the current directory

--- setup_biicode_project.py
+++ setup_biicode_project.py
@@ -5,14 +5,12 @@ import re, os, shutil, sys, subprocess
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
 
 def bii_user_project():
-  biiconfig = open(os.path.join(SCRIPTDIR, '..', 'biicode.conf'), 'r').read()
+  biiconfig = open(os.path.join(SCRIPTDIR, 'biicode.conf'), 'r').read()
   username = re.search('\[parent\]\n\s+([^/]+)/', biiconfig).group(1)
   projectname = re.search('\[parent\]\n\s+[^/]+/([^:]+)', biiconfig).group(1)
   return (username, projectname)
 
 BUILDFOLDER = sys.argv[1]
-
-print "Buildfolder: "+BUILDFOLDER
 
 (bii_username, bii_projectname) =  bii_user_project()
 subprocess.check_call(['bii', 'init', BUILDFOLDER])
