890b7296f70f1bacaf764ed29a8391a07dc739f0
Sebastian Messmer <mail@smessmer.de>
2015-02-20 20:33:51 +0100

    Removed install_boost.sh script because biicode can now handle boost

--- install_boost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-sudo apt-add-repository -y ppa:boost-latest/ppa
-sudo apt-get update -qq
-sudo apt-get install -qq $@
-
-# unset verbose option from above, in case this script is called with "source"
-set +v
--- test/install_boost_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ev
-
-# Test install_boost script with some boost libraries
-./install_boost.sh libboost-filesystem1.55-dev libboost-system1.55-dev
