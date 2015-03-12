8a28118aff410fffc797f9f629a56f8dbb6606b7
Sebastian Messmer <mail@smessmer.de>
2015-03-12 14:39:22 +0100

    Implement new setup_biicode_project (it's simpler now because of new biicode version)

---
+++ setup_biicode_project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+# Install biicode
+wget http://apt.biicode.com/install.sh
+chmod +x install.sh
+./install.sh
+rm install.sh
+bii setup:cpp
+
+# Init project
+bii init -l
+bii find
+
+# unset options from above, in case this script is called with "source"
+set +v
